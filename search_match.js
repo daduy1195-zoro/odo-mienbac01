7cf067b3c6735a838f0a9d41f67181557c69d93b
daduy1195-zoro <daduy1195-zoro@users.noreply.github.com>
2026-08-27 17:30:21 +0700

    chore: clean up debug scripts

--- search_match.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('C:/Users/MSI/Desktop/AI/Odo/index.html', 'utf8').split('\n');
-for (let i = 1734; i <= 1750; i++) {
-    console.log(i + ': ' + lines[i].trim());
-}

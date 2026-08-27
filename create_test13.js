5ba082362b8ee2dfe6ca830d042b8f20b69a60d3
daduy1195-zoro <daduy1195-zoro@users.noreply.github.com>
2026-08-27 16:15:22 +0700

    chore: remove test scripts

--- create_test13.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('test_parse12.js', 'utf8');
-code = code.replace('let currentUser={};', 'let currentUser={};\nfunction detectWH(s) { if(s.includes("hải phòng")) return "Hải Phòng"; if(s.includes("hải dương")) return "Hải Dương"; return ""; } function normalizeStr(s){return s;}');
-fs.writeFileSync('test_parse13.js', code);
--- create_test5.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('test_parse4.js', 'utf8');
-code = code.replace(/continue;/g, 'console.log("CONTINUED"); continue;');
-fs.writeFileSync('test_parse5.js', code);
